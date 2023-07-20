a18dd0134facdfe2f4b74ce0ee938adcd51fccb6
戊戌白羊 <wuxubaiyang@gmail.com>
2023-07-20 20:28:30 +0800

    完成测试验证服务器是否支持断点续传功能

--- lib/manage/download.dart
+++ lib/manage/download.dart
@@ -15,11 +15,7 @@ class DownloadManage extends BaseManage {
   DownloadManage._internal();
 
   @override
-  Future<void> init() async {
-    final a = await _supportPause(
-        'https://vip.ffzyread.com/20230706/13485_c3370ee5/2000k/hls/73a9cf475d2000000.ts');
-    print('');
-  }
+  Future<void> init() async {}
 
   // 判断是否支持断点续传
   Future<bool> _supportPause(String url) async {
@@ -28,7 +24,7 @@ class DownloadManage extends BaseManage {
       final resp = await Dio().get(url, options: options);
       if (resp.statusCode == 200) {
         final headers = resp.headers.map;
-        return ['range', 'Accept-Ranges', 'Content-Range']
+        return ['range', 'accept-ranges', 'content-range']
             .any(headers.containsKey);
       }
     } catch (e) {
