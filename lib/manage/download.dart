f66397d7befe85a84b9adbab0edc2abf568ea0be
戊戌白羊 <wuxubaiyang@gmail.com>
2023-07-20 21:30:18 +0800

    +1

--- lib/manage/download.dart
+++ lib/manage/download.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_hls_parser/flutter_hls_parser.dart';
 import 'package:jtech_anime/common/manage.dart';
 import 'package:jtech_anime/tool/log.dart';
 
@@ -16,6 +17,25 @@ class DownloadManage extends BaseManage {
 
   @override
   Future<void> init() async {}
+
+  // 解析m3u8文件并获取全部的下载记录
+  Future<Map<String, String>> _parseM3U8File(String url) async {
+    try {
+      final resp = await Dio().get(url);
+      if (resp.statusCode == 200) {
+        final uri = Uri.parse(url);
+        final parser = HlsPlaylistParser.create();
+        final playlist = parser.parseString(uri, resp.data);
+        if (playlist is HlsMasterPlaylist) {
+        } else if (playlist is HlsMediaPlaylist) {
+
+        }
+      }
+    } catch (e) {
+      LogTool.e('m3u8文件解析失败', error: e);
+    }
+    return {};
+  }
 
   // 判断是否支持断点续传
   Future<bool> _supportPause(String url) async {
