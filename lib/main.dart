df4396169a60dc1a1718b75419469d148c88705c
戊戌白羊 <wuxubaiyang@gmail.com>
2023-07-22 10:25:48 +0800

    +1

--- lib/main.dart
+++ lib/main.dart
@@ -42,8 +42,8 @@ class MyApp extends StatelessWidget {
       builder: (c, snap) => MaterialApp(
         title: Common.appName,
         theme: snap.data?.data,
-        debugShowCheckedModeBanner: false,
         navigatorKey: router.navigateKey,
+        debugShowCheckedModeBanner: false,
         onGenerateRoute: router.onGenerateRoute(
           routesMap: RoutePath.routes,
         ),
