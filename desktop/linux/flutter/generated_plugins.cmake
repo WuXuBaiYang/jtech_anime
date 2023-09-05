b646367edeed7b99cf6eb41adba972b15c7a758d
jianghan <wuxubaiyang@gmail.com>
2023-09-05 16:49:45 +0800

    整理项目目录结构

--- desktop/linux/flutter/generated_plugins.cmake
+++ desktop/linux/flutter/generated_plugins.cmake
@@ -3,32 +3,21 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  battery_plus
-  connectivity_plus
-  file_selector_windows
-  flutter_js
-  isar_flutter_libs
-  media_kit_libs_windows_video
-  media_kit_video
-  permission_handler_windows
-  screen_brightness_windows
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  media_kit_native_event_loop
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/linux plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
 endforeach(plugin)
 
 foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/windows plugins/${ffi_plugin})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/linux plugins/${ffi_plugin})
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
 endforeach(ffi_plugin)
