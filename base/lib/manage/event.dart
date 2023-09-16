f0d3f2daa79383cd2865fa7a98e38d2f0628a6db
戊戌白羊 <wuxubaiyang@gmail.com>
2023-09-17 07:26:46 +0800

    +1

--- base/lib/manage/event.dart
+++ base/lib/manage/event.dart
@@ -19,16 +19,15 @@ class EventManage extends BaseManage {
       : _streamController = StreamController.broadcast(sync: false);
 
   // 注册事件
-  Stream<T> on<T extends EventModel>() {
-    if (T == EventModel) {
-      return _streamController.stream as Stream<T>;
-    } else {
-      return _streamController.stream.where((event) => event is T).cast<T>();
-    }
-  }
+  Stream<T> on<T>() =>
+      _streamController.stream.where((event) => event is T).cast<T>();
+
+  // 注册值对比事件
+  Stream<T> onValue<T>(T value) =>
+      _streamController.stream.where((event) => event == value).cast<T>();
 
   // 发送事件
-  void send<T extends EventModel>(T event) => _streamController.add(event);
+  void send<T>(T event) => _streamController.add(event);
 
   // 销毁消息总线
   void destroy() => _streamController.close();
